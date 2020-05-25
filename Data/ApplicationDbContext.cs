d33d031bb903bca789521b9ec71ce46249905123
Stenio Neves Silva <stenioneves@ymail.com>
2020-05-25 17:20:40 -0300

    Contexto da aplicação

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using sistemE.Models;
+
+namespace sistemE.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Beneficiario>Beneficiarios {get; set;}
+        
+        public DbSet<Emprestimo>Emprestimos {get; set;}
+        
+        public DbSet<Situacao>Situacaos {get; set;}
+
+        public DbSet<Pagamento>Pagamentos{get;set;}
+    }
+}
